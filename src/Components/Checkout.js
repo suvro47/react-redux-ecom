c17365a916ffdd27e349f004d036c653f3534de4
suvro47 <suvro047@gmail.com>
2021-09-19 19:15:10 +0600

    checkout page modified

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -18,21 +18,24 @@ export default function Purchese() {
             <ul className="flex justify-between p-2">
               <li className="w-auto"> {index + 1}</li>
               <li className="w-auto">{title}</li>
-              <li className="w-auto"> Price : {price}</li>
-              <li className="w-auto">Quantity : {quantity}</li>
-              <li className="w-auto">Cost : {quantity * price}</li>
+              <li className="w-auto"> {price} $ </li>
+              <li className="w-auto"> x </li>
+              <li className="w-auto">{quantity} </li>
+              <li className="w-auto"> = </li>
+              <li className="w-auto">{quantity * price} $</li>
             </ul>
           </div>
         ))}
 
-        <div className="w-1/2 m-auto mt-4 border ">
+        <div className="flex justify-end w-1/2 p-2 m-auto mt-4 border">
           <hr></hr>
+          Total Cost : {cost.toFixed(2)} $
         </div>
 
-        <div className="flex flex-row justify-around w-1/2 p-2 m-auto">
-          <div className="px-2 py-1 mx-8 my-10 text-base text-black rounded shadow-2xl">
-            Total Cost : {cost.toFixed(2)} $
-          </div>
+        <div className="flex flex-row justify-between w-1/2 p-2 m-auto">
+          <button className="px-2 py-1 my-10 text-base text-white bg-blue-700 rounded shadow-2xl">
+            <Link to="/print"> Print </Link>
+          </button>
           <button className="px-2 py-1 my-10 text-base text-white bg-green-700 rounded shadow-2xl">
             <Link to="/purchese_request"> Purchese </Link>
           </button>
